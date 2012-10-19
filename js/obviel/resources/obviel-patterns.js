3b7e9642aa260e6c06c4ddac9f4545c54540483f
Christian Zagrodnick <cz@flyingcircus.io>
2012-10-19 09:55:05 +0200

    extracted js.obviel packaging from base obviel package

---
+++ js/obviel/resources/obviel-patterns.js
@@ -0,0 +1,35 @@
+/*global jQuery:true, templateUrl:true,
+  alert:true , browser:true, document:true, appUrl:true,
+  window:true
+*/
+(function($, obviel) {
+    
+    obviel.view({
+        iface: 'event',
+        ephemeral: true,
+        render: function() {
+            this.el.trigger(this.obj.name);
+        }
+    });
+    
+    obviel.view({
+        iface: 'redirect',
+        ephemeral: true,
+        render: function() {
+            this.el.render(this.obj.target);
+        }
+    });
+
+    obviel.view({
+        iface: 'multi',
+        ephemeral: true,
+        render: function() {
+            var el = this.el;
+            var objects = this.obj.objects || [];
+            $.each(objects, function(index, obj) {
+                el.render(obj);
+            });
+        }
+    });
+    
+})(jQuery, obviel);
